c831a74c6e5eb0919af1c0b21a9ea0258b317350
nvcexploder <benacker@gmail.com>
2014-04-25 13:39:59 -0700

    options. :/

--- makemehapi.js
+++ makemehapi.js
@@ -1,14 +1,30 @@
 #!/usr/bin/env node
 
-// Require modules
+const Workshopper = require('workshopper');
+const path        = require('path')
+const credits     = require('./credits');
+const menu        = require('./exercises/menu');
 
-var Workshopper = require('workshopper');
-var Path = require('path');
+const name        = 'makemehapi';
+const title       = 'REST WELL WITH HAPI';
+const subtitle    = '\x1b[23mSelect an exercise and hit \x1b[3mEnter\x1b[23m to begin';
+
+
+function fpath (f) {
+  return path.join(__dirname, f)
+}
 
 
 Workshopper({
-    name: 'makemehapi',
-    title: 'Make Me Hapi',
-    appDir: __dirname,
-    helpFile : Path.join(__dirname, 'help.txt')
-}).init();
+    name        : name,
+    title       : title,
+    subtitle    : subtitle,
+    exerciseDir : fpath('./exercises/'),
+    appDir      : __dirname,
+    helpFile    : fpath('help.txt'),
+    menu        : [ {
+        name    : 'credits',
+        handler : credits
+    } ],
+    menuOptions : {}
+});
