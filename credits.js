317b4f7e81c4bae0f49498c484fbb283700d16ba
nvcexploder <benacker@gmail.com>
2014-04-25 13:10:41 -0700

    Add credits. Modify Hello Hapi

---
+++ credits.js
@@ -0,0 +1,15 @@
+const Fs     = require('fs');
+const Path   = require('path');
+const Colors = require('colors-tmpl');
+
+
+function credits () {
+  Fs.readFile(Path.join(__dirname, './credits.txt'), 'utf8', function (err, data) {
+    if (err)
+      throw err
+
+    console.log(Colors(data))
+  })
+}
+
+module.exports = credits
