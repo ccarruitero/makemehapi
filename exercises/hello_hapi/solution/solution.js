115b959bd3541de081ad2b783d00dbc8e12960d8
nvcexploder <benacker@gmail.com>
2014-04-28 11:11:37 -0700

    Tabs to spaces

--- exercises/hello_hapi/solution/solution.js
+++ exercises/hello_hapi/solution/solution.js
@@ -1,10 +1,10 @@
 var Hapi = require('hapi');
 var server = Hapi.createServer('localhost', 8081);
 server.route({
-	method: 'GET',
-	path: '/',
-	handler: function (request, reply) {
-		reply('Hello Hapi');
-	}
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello Hapi');
+    }
 });
 server.start();
