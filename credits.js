70067a19b1ab1c018e1d275490148c4bd37f2551
nvcexploder <benacker@gmail.com>
2014-04-28 11:05:43 -0700

    wlp comments

--- credits.js
+++ credits.js
@@ -4,12 +4,15 @@ const Colors = require('colors-tmpl');
 
 
 function credits () {
-  Fs.readFile(Path.join(__dirname, './credits.txt'), 'utf8', function (err, data) {
-    if (err)
-      throw err
 
-    console.log(Colors(data))
-  })
+  Fs.readFile(Path.join(__dirname, './credits.txt'), 'utf8', function (err, data) {
+
+    if(err) {
+      throw err;
+    }
+
+    console.log(Colors(data));
+  });
 }
 
-module.exports = credits
+module.exports = credits;
