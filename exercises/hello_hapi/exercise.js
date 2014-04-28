8c1f6289b03814306b08d18c6b93ffc4e5c4bcb6
nvcexploder <benacker@gmail.com>
2014-04-28 10:59:57 -0700

    comments and semi-colons

--- exercises/hello_hapi/exercise.js
+++ exercises/hello_hapi/exercise.js
@@ -1,17 +1,53 @@
-var exercise      = require('workshopper-exercise')()
-  , filecheck     = require('workshopper-exercise/filecheck')
-  , execute       = require('workshopper-exercise/execute')
-  , comparestdout = require('workshopper-exercise/comparestdout')
+var exercise      = require('workshopper-exercise')();
+var filecheck     = require('workshopper-exercise/filecheck');
+var execute       = require('workshopper-exercise/execute');
+var comparestdout = require('workshopper-exercise/comparestdout');
+var compareserver = require('lib/comparestdout');
 
 
 // checks that the submission file actually exists
-exercise = filecheck(exercise)
+exercise = filecheck(exercise);
 
 // execute the solution and submission in parallel with spawn()
-exercise = execute(exercise)
+exercise = execute(exercise);
+
+//prepare the compareserver
+var compare_config = {
+    url: '/',
+    submission_port: 8080,
+    example_port: 8081
+};
 
 // compare stdout of solution and submission
-exercise = comparestdout(exercise)
+exercise = comparestdout(exercise);
 
 
-module.exports = exercise
+
+module.exports = exercise;
+
+// Require modules
+
+// var PassThrough = require('stream').PassThrough;
+// var hyperquest = require('hyperquest');
+
+
+// module.exports = function (run) {
+
+//     var submissionOut = new PassThrough();
+//     var solutionOut = new PassThrough();
+
+//     setTimeout(function () {
+
+//         hyperquest.get('http://localhost:8080').pipe(submissionOut);
+//         if (!run) {
+//             hyperquest.get('http://localhost:8081').pipe(solutionOut);
+//         }
+//     }, 500);
+
+//     return {
+//         submissionArgs: [8080],
+//         solutionArgs: [8081],
+//         a: submissionOut,
+//         b: solutionOut
+//     };
+// };
