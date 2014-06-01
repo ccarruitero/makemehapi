e5a9a872ffd4d41750679c29538a4e88136671f0
linclark <lin.w.clark@gmail.com>
2014-06-01 16:00:23 -0700

    Add exercise for proxies.

--- exercises/proxies/solution/solution.js
+++ exercises/proxies/solution/solution.js
@@ -1,1 +1,15 @@
-//proxy hapijs.com
+var Hapi = require('hapi');
+var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
+
+server.route({
+    method: 'GET',
+    path: '/proxy',
+    handler: {
+        proxy: {
+            host: '127.0.0.1',
+            port: 65535
+        }
+    }
+});
+
+server.start()
