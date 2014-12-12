51c5415325557fc90c743adf7164d29a1330480b
paulgrock <paulgrock@gmail.com>
2014-12-11 21:27:25 -0800

    Hapi 8 compatiblity updates to 6 and 7

--- exercises/helping/solution/solution.js
+++ exercises/helping/solution/solution.js
@@ -1,19 +1,17 @@
 var Hapi = require('hapi');
-var path = require('path')
-
-
-var options = {
-    views: {
-        path:  path.join(__dirname, 'templates'),
-        engines: {
-            html: require('handlebars')
-        },
-        helpersPath:  path.join(__dirname, 'helpers')
-    }
-};
-
-var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080), options);
-
+var path = require('path');
+var server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+server.views({
+    path: path.join(__dirname, 'templates'),
+    engines: {
+        html: require('handlebars')
+    },
+    helpersPath:  path.join(__dirname, 'helpers')
+});
 server.route({
     method: 'GET',
     path: '/',
