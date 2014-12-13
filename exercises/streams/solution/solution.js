33362732ff0c85a38fed2c141df3baacca2a4e26
paulgrock <paulgrock@gmail.com>
2014-12-13 13:51:10 -0800

    Switch to rot13-transform as suggested in #100.

--- exercises/streams/solution/solution.js
+++ exercises/streams/solution/solution.js
@@ -1,9 +1,13 @@
-var Fs = require('fs');
+var fs = require('fs');
 var path = require('path');
 var Hapi = require('hapi');
-var rot13 = require('rot13-stream')();
+var rot13 = require('rot13-transform');
+var server = new Hapi.Server();
 
-var server = Hapi.createServer('localhost', Number(process.argv[2] || 8081));
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
 
 server.route({
     method: "GET",
@@ -11,14 +15,11 @@ server.route({
     config: {
         handler: function (request, reply) {
 
-            var thisfile = Fs.createReadStream(path.join(__dirname, '/input.txt'));
-            
-            reply(thisfile.pipe(rot13));
+            var thisfile = fs.createReadStream(path.join(__dirname, '/input.txt'));
+
+            reply(thisfile.pipe(rot13()));
         }
     }
 });
 
 server.start();
-
-
-
