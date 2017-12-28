abd295defb20c78a8c4a4c7a6d62a6c9a523fe64
lenewman <lance.newman@elynxx.com>
2017-12-28 13:41:44 -0500

    updated the streams problem and solutions files.

--- exercises/streams/solution/solution.js
+++ exercises/streams/solution/solution.js
@@ -3,24 +3,27 @@ var Hapi = require('hapi');
 var Path = require('path');
 var Rot13 = require('rot13-transform');
 
-var server = new Hapi.Server();
+(async () => {
+    try {
+        const server = Hapi.Server({ 
+            host: 'localhost',
+            port: Number(process.argv[2] || 8080) 
+        });
+       
+        server.route({
+            path: '/',
+            method: 'GET',
+            config: {
+                handler: (request, h) => {
+                    var thisfile = Fs.createReadStream(Path.join(__dirname, 'input.txt'));
+                    return thisfile.pipe(Rot13()); 
+                }
+            }
+        });
 
-server.connection({
-    host: 'localhost',
-    port: Number(process.argv[2] || 8080)
-});
+        await server.start();
 
-server.route({
-    method: 'GET',
-    path: '/',
-    config: {
-        handler: (request, reply) => {
-            var thisfile = Fs.createReadStream(Path.join(__dirname, 'input.txt'));
-            reply(thisfile.pipe(Rot13()));
-        }
+    } catch (error) {
+        console.log(error);
     }
-});
-
-server.start((err) => {
-    if (err) throw err;
-});
+})();
