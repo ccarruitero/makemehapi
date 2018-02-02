57b8c81a9eeb44e7c75aafef5f0bf5bc1b59f83f
Matt Hernandez <maherna@microsoft.com>
2018-02-02 08:37:29 -0800

    Update outdated dependencies.
    form-data 2.2.0 -> 2.3.1; handlebars 4.0.0 -> 4.0.11;
    hyperquest 2.0.0 -> 2.1.3
    
    Also corrected formatting for the uploads solution.

--- exercises/uploads/solution/solution.js
+++ exercises/uploads/solution/solution.js
@@ -1,52 +1,52 @@
 (async () => {
-  try {
-      const Hapi = require('hapi');
-      const server = new Hapi.Server({
-          host: 'localhost',
-          port: Number(process.argv[2] || 8080)
-      });
+    try {
+        const Hapi = require('hapi');
+        const server = new Hapi.Server({
+            host: 'localhost',
+            port: Number(process.argv[2] || 8080)
+        });
 
-      server.route({
-          method: 'POST',
-          path: '/upload',
-          config: {
-              handler: (request, h) => {
-                  return new Promise((resolve, reject) => {
-                      let body = '';
-  
-                      request.payload.file.on('data', (data) => {
-                          body += data;
-                      });
-      
-                      request.payload.file.on('end', () => {
-                          let result = {
-                              description: request.payload.description,
-                              file: {
-                                  data: body,
-                                  filename: request.payload.file.hapi.filename,
-                                  headers: request.payload.file.hapi.headers
-                              }
-                          };
-      
-                          return resolve(JSON.stringify(result));
-                      });
+        server.route({
+            method: 'POST',
+            path: '/upload',
+            config: {
+                handler: (request, h) => {
+                    return new Promise((resolve, reject) => {
+                        let body = '';
 
-                      request.payload.file.on('error', (err) => {
-                          return reject(err);
-                      });
-                  });
-              },
-              payload: {
-                  output: 'stream',
-                  parse: true,
-                  allow: 'multipart/form-data'
-              }
-          }
-      });
+                        request.payload.file.on('data', (data) => {
+                            body += data;
+                        });
 
-      await server.start();
-  }
-  catch (error) {
-      console.log(error)
-  }
+                        request.payload.file.on('end', () => {
+                            let result = {
+                                description: request.payload.description,
+                                file: {
+                                    data: body,
+                                    filename: request.payload.file.hapi.filename,
+                                    headers: request.payload.file.hapi.headers
+                                }
+                            };
+
+                            return resolve(JSON.stringify(result));
+                        });
+
+                        request.payload.file.on('error', (err) => {
+                            return reject(err);
+                        });
+                    });
+                },
+                payload: {
+                    output: 'stream',
+                    parse: true,
+                    allow: 'multipart/form-data'
+                }
+            }
+        });
+
+        await server.start();
+    }
+    catch (error) {
+        console.log(error)
+    }
 })();
