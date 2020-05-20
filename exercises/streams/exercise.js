80c1bb46ae8c4a1af72895dc6d27284195c1385a
César Carruitero <ccarruitero@protonmail.com>
2020-05-19 22:20:16 -0500

    chore(stream): add missing verification for 404

--- exercises/streams/exercise.js
+++ exercises/streams/exercise.js
@@ -2,4 +2,14 @@ const exercise = require('../../lib/exercise')
 
 exercise.queryUrl = (port) => `http://localhost:${port}/`;
 
+exercise.addVerifyProcessor(function (callback) {
+  const { response, url } = this.submissionResult;
+  if (response.statusCode === 404) {
+    const msg = exercise.__('fail.page_not_found', { url });
+    exercise.emit('fail', msg);
+
+    callback(null, false);
+  }
+});
+
 module.exports = exercise;
